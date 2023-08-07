1dc2dd53eef6dc7419a11338acb8adf0f4d81641
ot-babs <toyosi.babs@gmail.com>
2023-08-07 21:37:03 +0100

    Created a static web interface & Implemented "Add new todo items"

--- web.py
+++ web.py
@@ -2,6 +2,12 @@ import streamlit as st
 
 import functions
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+    print(todo)
+
 todos = functions.get_todos()
 
 st.title("My Todo WebApp")
@@ -14,4 +20,7 @@ st.checkbox("Throw the trash.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo...")
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
+
+st.session_state
