3423ac68f7521f307bb161d7118c8f9bf2a96874
ot-babs <toyosi.babs@gmail.com>
2023-08-08 07:01:18 +0100

    Implemented checkbox for completed Todos

--- web.py
+++ web.py
@@ -1,26 +1,26 @@
 import streamlit as st
-
 import functions
 
+todos = functions. get_todos()
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
     print(todo)
 
-todos = functions.get_todos()
 
 st.title("My Todo WebApp")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-st.checkbox("Buy grocery.")
-st.checkbox("Throw the trash.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
-
-st.session_state
